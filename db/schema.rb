bdaec29c56f3491ca82b3b6700d6cf324989e83c
Brad Korman <brad.korman@remarque-creative.com>
2019-11-24 16:27:15 +0200

    added description migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_24_105018) do
+ActiveRecord::Schema.define(version: 2019_11_24_142208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_11_24_105018) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
