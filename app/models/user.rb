6057c7e8e7e25d4a9b9f07fc2cd3573be7e16f73
Omer Geva <ogeva97@gmail.com>
2019-11-25 21:08:14 +0200

    made option to change photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  before_create :default_values
-
+  # before_create :default_values
+  DEFAULT_PICTURE = 'https://i.kym-cdn.com/photos/images/newsfeed/001/452/271/0f9'
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :listings
+  mount_uploader :photo, PhotoUploader
 
-  def default_values
-    self.photo = 'https://i.kym-cdn.com/photos/images/newsfeed/001/452/271/0f9'
-  end
+  # def default_values
+  #   self.remote_photo_url = 'https://i.kym-cdn.com/photos/images/newsfeed/001/452/271/0f9'
+  # end
 end
