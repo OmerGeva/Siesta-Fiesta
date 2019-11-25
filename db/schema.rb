515ae8227ba6cfb5e87a507d7086a5a2ee1acb03
Omer Geva <ogeva97@gmail.com>
2019-11-25 11:36:30 +0200

    added authorization for new listing and on show page now appears the listings actual photo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_24_154835) do
+ActiveRecord::Schema.define(version: 2019_11_24_155238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
