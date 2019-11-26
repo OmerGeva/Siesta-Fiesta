d2173018901307a459b3bfef0afb064d5cc09d24
Brad Korman <brad.korman@remarque-creative.com>
2019-11-26 12:08:01 +0200

    Added leave review form for listings

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :listings do
     resources :bookings, only: [:new, :create]
+    resources :reviews, only: [:create]
   end
   namespace :account do
     resources :bookings, only: [:index]
