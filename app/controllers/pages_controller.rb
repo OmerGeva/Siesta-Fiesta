465839526a767abcb675eec805aff865d65e4243
Omer Geva <ogeva97@gmail.com>
2019-11-28 15:30:10 +0200

    ordered front page by reviews

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,24 @@ class PagesController < ApplicationController
 
   def home
     # @listings = Listing.joins(:reviews).order('avg_rating DESC')
-    @listings = Listing.all.limit(8)
+    avg_hash = {}
+    top_array = []
+    avg_counter = 0
+    count = 5
+    Listing.all.each do |listing|
+      listing.reviews.each do |review|
+        avg_counter += review.rating
+      end
+      avg_hash[listing.title] = listing.reviews.count == 0 ? 0 : avg_counter/listing.reviews.count
+      avg_counter = 0
+    end
+
+    index_array = (avg_hash.sort_by { |el| el.reverse }).reverse
+    8.times do
+      top_array << Listing.find_by(title: index_array[avg_counter][0])
+      avg_counter += 1
+    end
+    @listings = top_array
     @new_listing = Listing.new
   end
 end
