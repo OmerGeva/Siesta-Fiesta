a9ec9663cc641c8d6f5ff89a85dab14459b7dc40
Omer Geva <ogeva97@gmail.com>
2019-11-28 12:47:06 +0200

    ajax for the reviews

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,8 @@ module SiestaFiesta
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
-    # Settings in config/environments/* take precedence over those specified here.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
+    # Settings  in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
