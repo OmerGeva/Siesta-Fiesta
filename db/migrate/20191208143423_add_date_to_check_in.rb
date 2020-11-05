6b85010123ce1ffe5a6bd7f2719019f7f43d101c
Omer Geva <ogeva97@gmail.com>
2020-11-05 15:23:05 +0200

    link in footer

---
+++ db/migrate/20191208143423_add_date_to_check_in.rb
@@ -0,0 +1,4 @@
+class AddDateToCheckIn < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_093825) do
+ActiveRecord::Schema.define(version: 2019_12_08_143423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
